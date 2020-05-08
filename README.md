c7044a7c7f1b165fa674a635c6109dfe62d1b4c2
David Egan <david@carawebs.com>
2020-05-08 20:22:24 +0100

    Hide output during entry of the secret.

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Polynomials in C
 Solve polynomials using C.
 
 @TODO Clean up inputs
+
+Entering Secrets Safely
+-----------------------
+See [this reference on termios][1].
+
+[1]: https://blog.nelhage.com/2009/12/a-brief-introduction-to-termios/
--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <termios.h>
 
 // get user input:
 // - A constant
@@ -8,16 +8,23 @@
 
 void inputData(int *vars, size_t size)
 {
+	struct termios echoOrig, echoOff;
+	tcgetattr(0, &echoOrig);
+	echoOff = echoOrig;
+	echoOff.c_lflag &= ~ECHO;
+
 	char *inputs[] = {
 		"Please enter the secret integer: ",
 		"Please enter the required number of shares: ",
 		"Please enter the threshold: "
 	};
-	
+
 	char *line = NULL, *endptr;
 	size_t len = 0;
 	ssize_t lineSize = 0;
 	for (size_t i = 0; i < sizeof(inputs) / sizeof(*inputs) || i < size; i++) {
+		if (i == 0) tcsetattr(0, TCSANOW, &echoOff);
+		if (i == 1) tcsetattr(0, TCSANOW, &echoOrig);
 		printf("%s\n", inputs[i]);
 		lineSize = getline(&line, &len, stdin);
 		if (lineSize == 0) {
